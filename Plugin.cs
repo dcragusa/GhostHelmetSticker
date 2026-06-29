207dc47df43c0c4aaa122d26059a3a1770120d4a
dcragusa <dcragusa@yahoo.com>
2026-06-28 17:58:40 -0700

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using BBI.Unity.Game;
+using Carbon.Localization.Core;
+
+namespace GhostHelmetSticker;
+
+[BepInPlugin("com.dcragusa.ghosthelmetsticker", "Ghost Helmet Sticker", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo("Ghost Helmet Sticker by dcragusa - https://github.com/dcragusa/GhostHelmetSticker");
+        var harmony = new Harmony("com.dcragusa.ghosthelmetsticker");
+        harmony.PatchAll();
+        Logger.LogInfo("Plugin loaded!");
+    }
+}
+
+[HarmonyPatch(typeof(StickerAsset), "AmountRequiredForSticker", MethodType.Getter)]
+public class PatchStickerAmountGetter
+{
+    public static void Postfix(StickerAsset __instance, ref double __result)
+    {
+        if (__instance.name == "GhostShip_Helmet_StickerAsset")
+        {
+            __result = 2;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(SingleLanguageCacheLocalizationDatabase), "TryFindEntry", [typeof(ulong), typeof(string)], [ArgumentType.Normal, ArgumentType.Ref])]
+public class DatabaseLookupPatch
+{
+    public static void Postfix(bool __result, ulong id, ref string entry)
+    {
+        if (__result && id == 9692 && entry != null)
+        {
+            entry = entry.Replace("3", "2");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# GhostHelmetSticker
+
+The shiny Ghost Helmet sticker requires you to get 6 helmet pickups, but there are only 5 in the game! 
+
+This is a small BepInEx mod that lowers the requirement for the Ghost Helmet sticker to 2 helmets instead of 3. Thus, crafting the shiny sticker will only require a total of 4 helmet pickups.
+
+## Installation
+
+- Install BepInEx for Hardspace: Shipbreaker if you haven't already.
+- Drop GhostHelmetSticker.dll into BepInEx/plugins/.
+- Launch the game.
